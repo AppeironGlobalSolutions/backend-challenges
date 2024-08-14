715de059786bc82729e457b138b4d1c3c6074996
NicolasMelluso <nicolas.melluso@globant.com>
2024-08-14 14:13:12 -0300

    add nodemailer to allow the system send mails and notify the user when a new task was assigned or a task was modified

---
+++ domain/services/notification.service.ts
@@ -0,0 +1,25 @@
+// services/notificationService.ts
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendNotification = async (to: string, subject: string, text: string) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
--- infrastructure/controllers/task.controller.ts
+++ infrastructure/controllers/task.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { TaskService } from '../../domain/services/task.service';
+import { sendNotification } from '../../domain/services/notification.service';
+import { User } from '../../domain/entities/user.entity';
 
 const taskService = new TaskService();
 
@@ -18,11 +20,17 @@ export const createTask = [
       return res.status(400).json({ errors: errors.array() });
     }
 
+    const user = await User.findById( (req as any).user.userId);
+      if (!user) {
+        return res.status(404).json({ message: 'Assigned user not found' });
+      }
+    
     const { title, description, dueDate, status, projectId, assignedTo } = req.body;
 
     try {
       const task = await taskService.createTask({ title, description, dueDate, status, projectId, assignedTo });
       res.status(201).json(task);
+      await sendNotification(user.email, 'New Task Assigned', `You have been assigned a new task: ${title}`);
     } catch (err) {
       res.status(500).json({ message: 'Error creating task', error: err });
     }
@@ -53,8 +61,17 @@ export const getUserTasks = async (req: Request, res: Response) => {
 export const updateTask = async (req: Request, res: Response) => {
   const { id } = req.params;
 
+  const user = await User.findById( (req as any).user.userId);
+      if (!user) {
+        return res.status(404).json({ message: 'Assigned user not found' });
+      }
+    
+    const { title } = req.body;
+
+
   try {
     const updatedTask = await taskService.updateTask(id, req.body);
+    await sendNotification(user.email, 'Task Updated', `A task assigned to you has been updated: ${title}`);
     res.json(updatedTask);
   } catch (err) {
     res.status(500).json({ message: 'Error updating task', error: err });
